a1307b94c5a6ea6e8344b808dd7c41f0de354971
omaryasser <oyasser826@gmail.com>
2016-09-29 20:15:03 +0200

    Update 11195 - Another n-Queen Problem.cpp

--- 11195 - Another n-Queen Problem.cpp
+++ 11195 - Another n-Queen Problem.cpp
@@ -46,3 +46,62 @@ int main() {
 
 	return 0;
 }
+
+
+#include <bits/stdc++.h>
+#define FOR(x,y) for (int x = 0 ; x < y ; ++ x)
+#define FOR1(x,y,z) for (int x = y ; x < z ; ++ x)
+#define FOR2(x,y,z) for (int x = y ; x >= z ; -- x)
+#define pb(x) push_back(x)
+#define clr(x,y) memset (x , y , sizeof(x))
+#define bug cout<<"HERE"<<endl;
+#define tr1(x) cout << x << endl;
+#define tr2(x , y) cout << x << " " << y <<  endl;
+#define SSTR( x ) static_cast< std::ostringstream & >( \
+        ( std::ostringstream() << std::dec << x ) ).str()
+#define fast 	ios_base::sync_with_stdio(0); cin.tie(0);
+#define inp freopen("input.txt", "r", stdin);
+#define out freopen("output.txt", "w", stdout);
+using namespace std;
+
+typedef long long ll;
+
+string arr[16];
+ll cnt ;
+int n ;
+int maxx ;
+map <int , int> mapp;
+int cntBIts (int num) {
+	int res = 0;
+	while (num > 0) {
+		if (num & 1) res ++ ;
+		num /= 2;
+	}
+	return res ;
+}
+void solve (int rw , int ld , int rd) {
+	if (rw == maxx) {cnt ++ ; return ;}
+	int pos = (~ (rw | ld | rd)) & maxx ;
+	while (pos) {
+		int nw = pos & (- pos);
+		if (arr[cntBIts(rw)][mapp[nw]] == '.')
+			solve (rw | nw , (ld | nw) >> 1 , (rd | nw) << 1);
+		pos -= nw ;
+	}
+}
+int main() {
+
+	fast
+	int tt = 1;
+	FOR (i , 17) mapp[1 << i] = i ;
+	while ((cin >> n) && (n)) {
+		maxx = (1 << n) - 1;
+		FOR (i , n) cin >> arr[i] ;
+		cnt = 0 ;
+		solve (0 , 0 , 0);
+		cout << "Case " << tt++ << ": " << cnt << "\n";
+	}
+
+	return 0;
+}
+
